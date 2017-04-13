6a0bda74efd0093335982e9e13db85de80db354d
Olaf Japp <japp.olaf@gmail.com>
2017-04-13 17:12:33 +0200

    rename AppImage

--- Deploy/AppImage/create.sh
+++ Deploy/AppImage/create.sh
@@ -3,4 +3,5 @@ export LD_LIBRARY_PATH
 PATH=$PATH:/home/olaf/Qt/5.8/gcc_64/bin
 export PATH
 /home/olaf/linuxdeployqt/linuxdeployqt-continuous-x86_64.AppImage AnimationMaker -appimage
+mv AnimationMaker-x86_64.AppImage AnimationMaker-x86_64-1.0-alpha.AppImage
 
