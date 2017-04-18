871f6fed038333b634226eb3c96dfc0fe7d04a9f
Olaf Japp <japp.olaf@gmail.com>
2017-04-18 12:06:28 +0200

    Distribution name changed

--- Deploy/AppImage/create.sh
+++ Deploy/AppImage/create.sh
@@ -3,5 +3,5 @@ export LD_LIBRARY_PATH
 PATH=$PATH:/home/olaf/Qt/5.8/gcc_64/bin
 export PATH
 /home/olaf/linuxdeployqt/linuxdeployqt-continuous-x86_64.AppImage AnimationMaker -appimage
-mv Application-x86_64.AppImage AnimationMaker-x86_64-1.0-alpha.AppImage
+mv Application-x86_64.AppImage AnimationMaker-Linux-x86_64-1.0-alpha.AppImage
 
