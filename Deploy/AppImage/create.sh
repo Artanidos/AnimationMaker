b1d83f5fe8c447c9d8dc1613235281ae60cb3fc6
Olaf Japp <japp.olaf@gmail.com>
2017-05-30 14:30:58 +0200

    ceate file dropped

--- Deploy/AppImage/create.sh
+++
@@ -1,7 +0,0 @@
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/olaf/Qt/5.8/gcc_64/lib
-export LD_LIBRARY_PATH
-PATH=$PATH:/home/olaf/Qt/5.8/gcc_64/bin
-export PATH
-/home/olaf/linuxdeployqt/linuxdeployqt-continuous-x86_64.AppImage AnimationMaker -appimage
-mv Application-x86_64.AppImage AnimationMaker-Linux-x86_64-1.1.AppImage
-
