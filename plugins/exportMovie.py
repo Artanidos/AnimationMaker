059fd606a328dbd20f253cbcba3640b0ca46107c
Olaf Japp <japp.olaf@gmail.com>
2017-06-14 01:10:21 +0200

    py plugins added

---
+++ plugins/exportMovie.py
@@ -0,0 +1,14 @@
+def displayName():
+	return "Movie"
+
+def filter():
+	return "Video format (*.mpg *.mp4 *.avi *.gif);;All Files (*)"
+
+def title():
+	return "Export Animation to Movie"
+
+def type():
+	return "EXPORT_MOVIE"
+
+def exportMovie(filename, view, length, fps, bar):
+	return
