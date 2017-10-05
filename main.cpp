9c9e04ddc8b389c09a3af6be92bd5d7be546ce3a
Olaf Japp <japp.olaf@gmail.com>
2017-10-05 20:11:45 +0200

    version changed

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     QCoreApplication::setApplicationName("AnimationMaker");
-    QCoreApplication::setApplicationVersion("1.4.1");
+    QCoreApplication::setApplicationVersion("1.5");
     QCoreApplication::setOrganizationName("Artanidos");
 
     QFont newFont("Sans Serif", 10);
